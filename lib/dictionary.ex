2e8f5977ea23d03d2c9494c9220ac247ded8192e
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-20 13:23:17 -0500

    Refactor Into Pipelines * use the pipeline operator (two characters.. wtf) * first arity parameter explicitly precedes pipelines for subsequent functions * functional composition is just chaining methods basically * pattern matching seems like their fancy way of saying you have to use x and y in a certain order given arity of two

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -17,11 +17,13 @@ defmodule Dictionary do
   end
 
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list = String.split(contents, ~r{\n})
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r{\n})
   end
 end
