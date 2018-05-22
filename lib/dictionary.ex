a902816c74f314bd2cf1d61229620a98a6abeae5
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-22 14:28:49 -0500

    challenges; admittedly the twins function still doesnt make sense; pattern matching

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -26,4 +26,23 @@ defmodule Dictionary do
     |> File.read!()
     |> String.split(~r{\n})
   end
+
+  def swap({a,b}) do
+    IO.puts "{ #{b}, #{a} }"
+  end
+
+  # not really if/else, but still conditional
+  def twins_conditional(a, b) do
+    div(a,b) == 1 && rem(a,b) == 0
+  end
+
+  def twins(a, a) do
+    true
+  end
+
+  def twins(_, _) do
+    false
+  end
+  #twins(4, 5) # => false
+  #twins(5, 5) # => true
 end
